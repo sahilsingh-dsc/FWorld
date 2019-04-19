7a20e8e3fedd93f1274d256af41d8428dd0efece
sj670 <49678771+sj670@users.noreply.github.com>
2019-04-19 18:33:48 +0530

    commit@_19_04_19_6_30_completescreen

--- app/src/main/java/com/example/fwprld/ui/activities/ChooseLanguageActivity.java
+++ app/src/main/java/com/example/fwprld/ui/activities/ChooseLanguageActivity.java
@@ -1,27 +1,48 @@
 package com.example.fwprld.ui.activities;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.example.fwprld.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class ChooseLanguageActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+    DatabaseReference langRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
          setContentView(R.layout.activity_choose_language);
-          ImageView imgEnglish = findViewById(R.id.imgEnglish);
+         ImageView imgEnglish = findViewById(R.id.imgEnglish);
+        mAuth = FirebaseAuth.getInstance();
          imgEnglish.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                FirebaseUser user = mAuth.getCurrentUser();
+                String authid = user.getUid();
+                langRef = FirebaseDatabase.getInstance().getReference();
+                langRef.child("USER_DATA").child(authid).child("selected_language").setValue("English");
                 Intent mainIntent = new Intent(ChooseLanguageActivity.this, MainActivity.class);
                 startActivity(mainIntent);
                 finish();
             }
         });
+
     }
+
 }
--- app/src/main/java/com/example/fwprld/ui/activities/SplashActivity.java
+++ app/src/main/java/com/example/fwprld/ui/activities/SplashActivity.java
@@ -3,35 +3,79 @@ package com.example.fwprld.ui.activities;
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.CountDownTimer;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.example.fwprld.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class SplashActivity extends AppCompatActivity {
-    ImageView bounceImage;
+    ImageView bounceImage,connect;
+    private FirebaseAuth mAuth;
+    String id;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
         @SuppressLint("ResourceType") final Animation animBounce = AnimationUtils.loadAnimation(getApplicationContext(), R.animator.boun);
+        @SuppressLint("ResourceType") final Animation animBounce1 = AnimationUtils.loadAnimation(getApplicationContext(), R.animator.blink);
         bounceImage = (ImageView)findViewById(R.id.imagebounce);
+        connect= (ImageView)findViewById(R.id.connect);
+        connect.startAnimation(animBounce1);
         bounceImage.startAnimation(animBounce);
-        new CountDownTimer(4000,1000) {
+        mAuth = FirebaseAuth.getInstance();
+        mAuth.signInAnonymously()
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            assert user != null;
+                            String authid = user.getUid();
+                            DatabaseReference langRef = FirebaseDatabase.getInstance().getReference();
+                            langRef.child("USER_DATA").child(authid).child("selected_language").addValueEventListener(new ValueEventListener() {
+                                @Override
+                                public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                                    if (dataSnapshot.exists()){
+                                        Intent mainIntent = new Intent(SplashActivity.this, MainActivity.class);
+                                        startActivity(mainIntent);
+                                        finish();
+                                    }else {
+                                        Intent mainIntent = new Intent(SplashActivity.this, ChooseLanguageActivity.class);
+                                        startActivity(mainIntent);
+                                        finish();
+                                    }
+                                }
 
-            @Override
-            public void onFinish() {
-                Intent chooseLangIntent = new Intent(SplashActivity.this, ChooseLanguageActivity.class);
-                startActivity(chooseLangIntent);
-                finish();
-            }
-            @Override
-            public void onTick(long millisUntilFinished) {
+                                @Override
+                                public void onCancelled(@NonNull DatabaseError databaseError) {
 
-            }
-        }.start();
+                                }
+                            });
+
+                        } else {
+
+                        }
+
+
+                    }
+                });
+
     }
 }
