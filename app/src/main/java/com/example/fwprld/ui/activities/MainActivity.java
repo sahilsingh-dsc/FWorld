a474f8eca3ad77eff55172b6ea4456fcee787491
sj670 <49678771+sj670@users.noreply.github.com>
2019-04-19 19:17:15 +0530

    commit@_19_04_19_7_20_chngesdone

--- app/src/main/java/com/example/fwprld/ui/activities/MainActivity.java
+++ app/src/main/java/com/example/fwprld/ui/activities/MainActivity.java
@@ -2,24 +2,33 @@ package com.example.fwprld.ui.activities;
 
 import android.content.Context;
 import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
 import android.widget.AdapterViewFlipper;
 import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.SearchView;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 import com.example.fwprld.R;
 import com.example.fwprld.adapters.MyCustomPager;
 import com.example.fwprld.adapters.RecommendAdapter;
 import com.example.fwprld.models.Recommend;
 import com.example.fwprld.ui.activities.LoginActivity;
+
+import org.w3c.dom.Text;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -31,6 +40,8 @@ public class MainActivity extends AppCompatActivity {
     RecyclerView recyclerView;
     List<Recommend> recommendList;
     private MyCustomPager adapter;
+    int count=0;
+    AlertDialog alertDialog;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -40,7 +51,8 @@ public class MainActivity extends AppCompatActivity {
         profile.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(getApplicationContext(), LoginActivity.class));
+                Show();
+
             }
         });
         adapterViewFlipper=(AdapterViewFlipper) findViewById(R.id.viewpager);
@@ -77,43 +89,68 @@ public class MainActivity extends AppCompatActivity {
         //setting adapter to recyclerview
         recyclerView.setAdapter(adapter1);
     }
-   /* class MyCustomPager extends BaseAdapter {
-        Context otx;
-        int [] images;
-        LayoutInflater layoutInflater;
-
-        public MyCustomPager(Context otx,int []myimages){
-            this.otx=otx;
-            this.images= myimages;
-            this.layoutInflater=LayoutInflater.from(otx);
-
-        }
-        @Override
-        public int getCount() {
-            return images.length;
-
-        }
-
-        @Override
-        public Object getItem(int i) {
-            return null;
-        }
-
-        @Override
-        public long getItemId(int i) {
-            return 0;
-        }
-
-        @Override
-        public View getView(int i, View view, ViewGroup viewGroup) {
-            view=layoutInflater.inflate(R.layout.slideimage,null);
-            ImageView imageView=(ImageView)view.findViewById(R.id.slideimage);
-            imageView.setImageResource(images[i]);
-            return view;
-        }
+    private void Show() {
+        //Creating a LayoutInflater object for the dialog box
+        final LayoutInflater li = LayoutInflater.from(MainActivity.this);
+        //Creating a view to get the dialog box
+        View confirmDialog = li.inflate(R.layout.dialogue_layout, null);
 
 
+        //Initizliaing confirm button fo dialog box and edittext of dialog box
 
+        final Button btnsub = (Button) confirmDialog.findViewById(R.id.btnsub);
+        btnsub.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), LoginActivity.class));
+            }
+        });
+        final TextView close=(TextView)confirmDialog.findViewById(R.id.close);
+        close.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                alertDialog.dismiss();
 
-    }*/
+            }
+        });
+        final TextView moreways=(TextView)confirmDialog.findViewById(R.id.more);
+        final LinearLayout linearLayout=(LinearLayout)confirmDialog.findViewById(R.id.loginly);
+        moreways.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (count==0){
+                    linearLayout.setVisibility(View.VISIBLE);
+                    count=1;
+                }else{
+                    linearLayout.setVisibility(View.GONE);
+                    count=0;
+                }
+
+            }
+        });
+
+        AlertDialog.Builder alert = new AlertDialog.Builder(MainActivity.this);
+        alert.setView(confirmDialog);
+        // alert.setTitle("Select Donor");
+
+        //Creating an alert dialog
+       alertDialog = alert.create();
+
+        //Displaying the alert dialog
+
+        WindowManager.LayoutParams wmlp =  alertDialog.getWindow().getAttributes();
+
+        wmlp.gravity = Gravity.BOTTOM ;
+        wmlp.width = WindowManager.LayoutParams.MATCH_PARENT;
+        wmlp.height = WindowManager.LayoutParams.WRAP_CONTENT;
+       // wmlp.x = 100;   //x position
+       // wmlp.y = 0;   //y position
+        alertDialog.getWindow().setAttributes(wmlp);
+
+        alertDialog.show();
+
+    }
+
 }
+
