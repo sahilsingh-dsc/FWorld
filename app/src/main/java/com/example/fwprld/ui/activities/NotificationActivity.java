3f215f1b5b7283a9516053330a9f3e3a5413137d
surbhi91096 <surbhis9009@gmail.com>
2019-05-10 18:59:47 +0530

    Commit_10_5_2019_6_58_gift_notification

---
+++ app/src/main/java/com/example/fwprld/ui/activities/NotificationActivity.java
@@ -0,0 +1,17 @@
+package com.example.fwprld.ui.activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.fwprld.R;
+
+public class NotificationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notification);
+
+
+    }
+}
